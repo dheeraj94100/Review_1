fa9fe613aff5d42959ec91f728c1182927cd72c6
Dheeraj Sharma <dheeraj9410096874@gmail.com>
2025-01-28 11:38:16 +0530

    Task 2 completed Successfully.

--- task2.cs
+++ task2.cs
@@ -4,22 +4,26 @@ class task2
 {
     static void Main()
     {
-        int arr[] = {123, 124, 132, 156, 198, 144};
-        int ans[] = {0, 0, 0};
+        int []arr = {123, 124, 132, 156, 198, 144};
+
+        // 3 * 4 * 2 * 6 * 8 * 4 = 4608
+        // 2 * 2 * 3 * 5 * 9 * 4 = 2160
+        // 1 * 1 * 1 * 1 * 1 * 1 = 1
+        int []ans = {0, 0, 0};
         for(int i = 0; i < 3; i++)
         {
             int temp = 1;
             for(int j = 0; j < 6; j++)
             {
                 int rem = arr[j] % 10;
+                temp = temp * rem;
                 arr[j] /= 10;
-                temp *= rem;
             }
             ans[3 - i - 1] = temp;
         }
         for(int i = 0; i < 3; i++)
         {
-            Console.WriteLine(ans[i] + "   ");
+            Console.WriteLine(ans[i]);
         }
     }
 }
